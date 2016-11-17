91a9f2de251f7a9a2ba735d95b183202a6a5596b
Barry Howard <BarryLeeHoward@gmail.com>
2016-11-17 15:14:55 -0500

    added comment controllers

--- app/Http/Controllers/CommentController.js
+++ app/Http/Controllers/CommentController.js
@@ -2,6 +2,52 @@
 
 class CommentController {
 
+	* create (request, response) {
+		let data = request.only('content');
+		data.likes = 0;
+		let postsId = request.param('post_id');
+		data.posts_id = postId;
+
+		let comment = yield Comment.create(data)
+
+		comment.save()
+		response.status(201).json(comment)
+	}
+
+	* delete (request, response){
+		let comment_id = request.param("comment_id")
+
+		let comment = yield Comment.findBy('id', comment_id)
+
+		if (!comment){
+			response.status(404).json({text: "Comment not found"})
+		} else {
+	  		yield comment.delete()
+	  		response.status(201).json({text: "Comment deleted!", comment: comment})
+		}
+
+	}
+
+	* show (request, response){
+		let postId = request.param('post_id')
+		const comment_list = yield Comment.query().table('comments')
+		.where('posts_id', postId)
+		.orderBy('likes', 'desc')
+		.orderBy('created_at', 'desc')
+		response.json(comment_list)
+	}
+
+	* update(request, response){
+		let commentId = request.param('comment_id')
+		let comment = yield Comment.findBy('id', commentId)
+		let data = request.only('content')
+
+		comment.fill(data)
+
+		yield comment.save()
+		response.status(201).json(comment)
+	}
+
 }
 
 module.exports = CommentController
